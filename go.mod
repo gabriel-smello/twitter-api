1de4f5a86f862938af82aa64417708720b789e6a
gabriel-smello <gabriellsurf09@gmail.com>
2022-04-21 18:28:50 -0300

    set

--- go.mod
+++ go.mod
@@ -1,8 +1,8 @@
-module github.com/gabriel-smello/twitter-api
+module twitter-api
 
-go 1.18
+go 1.13
 
 require (
-	github.com/dghubble/go-twitter v0.0.0-20220413154426-14d8abde2e80 // indirect
-	github.com/dghubble/oauth1 v0.7.1 // indirect
+	github.com/dghubble/go-twitter v0.0.0-20220413154426-14d8abde2e80
+	github.com/dghubble/oauth1 v0.7.1
 )
--- main.go
+++ main.go
@@ -1,27 +1,43 @@
 package main
 
 import (
-	"fmt"
+	"log"
 
-	"github.com/dghubble/go-twitter"
+	"github.com/dghubble/go-twitter/twitter"
 	"github.com/dghubble/oauth1"
 )
 
-func main(){
-	CONSUMER_KEY = "2Nmc8tm1YxkLAqU8JtyYpZqMK"
-	CONSUMER_SECRET_KEY = "xQcd9lBMXElN3q4LWUQToxmN2bBbcsUNWka3D2wOl7sOm9SdJR"
-	TOKEN_KEY = "1517181806726033408-9KG1dcdacVJ8a4sZwwbdemZw5hGsIW"
-	TOKEN_SECRET_KEY = "YUp09dSymvNVJXlI4mCB31x3voycMJTNEzvO1Kmr7PSYi"
+func main() {
+	CONSUMER_KEY := "2Nmc8tm1YxkLAqU8JtyYpZqMK"
+	CONSUMER_SECRET_KEY := "xQcd9lBMXElN3q4LWUQToxmN2bBbcsUNWka3D2wOl7sOm9SdJR"
+	TOKEN_KEY := "1517181806726033408-9KG1dcdacVJ8a4sZwwbdemZw5hGsIW"
+	TOKEN_SECRET_KEY := "YUp09dSymvNVJXlI4mCB31x3voycMJTNEzvO1Kmr7PSYi"
 
 	config := oauth1.NewConfig(CONSUMER_KEY, CONSUMER_SECRET_KEY)
 	token := oauth1.NewToken(TOKEN_KEY, TOKEN_SECRET_KEY)
 
-	httpClient := config.Client(oauth1.NoContext,  token)
+	httpClient := config.Client(oauth1.NoContext, token)
 	client := twitter.NewClient(httpClient)
 
-	user, _, err := client.Accounts.VerifyCredentials(&twitter.AccountVerifyParams{})
-	if err != nil{
-		fmt.Printf("err: %v\n", err)
+	/*	tweet, _, err := client.Statuses.Update("uma mensagem qualquer", nil)
+		if err != nil {
+			log.Fatal((err))
+		}
+
+		log.Print(tweet.Text)
+	*/
+
+	tweets, _, err := client.Search.Tweets(&twitter.SearchTweetParams{
+		Query:  "guerra",
+		Count:  5,
+		Locale: "BR",
+	})
+	if err != nil {
+		log.Fatal(err)
 	}
-	fmt.Printf("Account: @%s (%s)\n", user.ScreenName, user.Name)
+
+	for _, val := range tweets.Statuses {
+		log.Print("Nome do Usuario: ", val.User.Name)
+		log.Print("Tweet: ", val.Text, "\n\n")
+	}
 }
