9f2eb152b4d803dd3fd2c1de8fd4afda8c340d94
gabriel-smello <gabriellsurf09@gmail.com>
2022-04-21 14:52:03 -0300

    new

--- main.go
+++ main.go
@@ -1,7 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"os"
 
-func main() {
-    fmt.Println("Hello, World!")
+	"github.com/dghubble/go-twitter"
+	"github.com/dghubble/oauth1"
+)
+
+func main(){
+	config := oauth1.NewConfig(os.Getenv("CONSUMER_KEY"), os.Getenv("CONSUMER_SECRET_KEY"))
+	token := oauth1.NewToken(os.Getenv("TOKEN_KEY"), os.Getenv("TOKEN_SECRET_KEY"))
+	httpClient := config.Client(oauth1.NoContext, token)
+
+	client := twitter.NewClient(httpClient)
+	
 }
